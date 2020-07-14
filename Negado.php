aea02430025d2b6012582cdbf9eb01b276dad7da
nazuki8540 <matheus.ghotme42@gmail.com>
2020-07-14 19:47:09 -0300

    Read me

---
+++ Negado.php
@@ -0,0 +1,26 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>Sr.Miyagi - Login</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
+	<script src="https://kit.fontawesome.com/a81368914c.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<center>
+<h1>Acesso negado! Você não tem permissão para essa pagina!</h1>
+	<img src="undraw_secure_login_pdn4 (1).svg" width=50% >
+	<div class="container">
+		
+		
+				
+        </div>
+    </div>
+    </center>
+    <script type="text/javascript" src="js/main.js"></script>
+</body>
+</html>
